98869660b87af64c4a6983ccc46b976f71fb5f70
Cathrine <cathrinekamasho@gmail.com>
2025-07-07 23:58:45 +0200

    installed bootstrap, created the search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  const APP_ID = "b6292b0e";
+  const APP_KEY = "3a94401803966455125c2d01a8868570	";
 
-  
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="Submit">
+          Search
+          </button>
+      </form>
     </div>
   );
 }
