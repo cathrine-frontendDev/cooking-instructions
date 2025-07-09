2041cd832d09843590ab12603133bee1b754051c
Cathrine <cathrinekamasho@gmail.com>
2025-07-09 16:37:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
   const APP_KEY = "3a94401803966455125c2d01a8868570";
 
   const [recipes, setRecipes] =useState([]);
-
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
-    getRecipes ();
-  }, []);
+    getRecipes();
+  }, [search]);
 
     const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?app_id=${APP_ID}&app_key=${APP_KEY}`
@@ -20,16 +21,21 @@ function App() {
     setRecipes(data.hits);
   };
 
+  const upDateSearch = e => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"/>
+        <input className="search-bar" type="text" value={search} />
         <button className="search-button" type="Submit">
           Search
           </button>
       </form>
       {recipes.map(recipe => (
         <Recipe 
+        key={recipe.recipe.label}
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories} 
         image={recipe.recipe.image}
